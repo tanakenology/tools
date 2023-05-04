79add7f750f45c6569c637ee814fa14323a4fe97
tanakenology <tanakenology@gmail.com>
2023-05-04 19:06:40 +0900

    (test) allow specifying an emulator for S3

--- apps/common_io/common_io/driver/csv_driver.py
+++ apps/common_io/common_io/driver/csv_driver.py
@@ -1,6 +1,8 @@
 import csv
+import os
 from collections.abc import Generator
 
+import boto3  # type: ignore
 import smart_open  # type: ignore
 
 
@@ -9,7 +11,19 @@ class CsvDriver:
     def read(
         cls, input_path: str, delimiter: str, row_as_list: bool
     ) -> Generator:
-        with smart_open.open(input_path, "r") as fin:
+        if input_path.startswith("s3://"):
+            tparams = {
+                "client": boto3.client(
+                    "s3",
+                    endpoint_url=os.getenv(
+                        "AWS_S3_ENDPOINT",
+                        default="https://s3.ap-northeast-1.amazonaws.com/",
+                    ),
+                )
+            }
+        else:
+            tparams = {}
+        with smart_open.open(input_path, "r", transport_params=tparams) as fin:
             if row_as_list:
                 yield from csv.reader(fin, delimiter=delimiter)
             else:
